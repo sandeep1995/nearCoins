c72735544d91b65b5adec7a018d38c06365fc0ee
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2016-07-24 14:28:04 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,34 +15,36 @@ router.get('/add/:id', function(req, res) {
 
 
 router.post('/process', function(req, res){
-	console.log(req.body);
+	res.json(req.body);
 });
 
 router.get('/redirect', function (req, res){
 	var payment_request_id = req.query.payment_request_id;
 	var payment_id = req.query.payment_id;
 	request.get('https://www.instamojo.com/api/1.1/payment-requests/'+payment_request_id+'/', {headers: headers}, function(error, response, body){
-	  if(!error && response.statusCode == 200){
-	    console.log(body);
+	  if(error){
+	    return res.json(error);
 	  }
+	  res.json(body);
 	});
 
 	request.get('https://www.instamojo.com/api/1.1/payment-requests/'+payment_request_id+'/'+payment_id, {headers: headers}, function(error, response, body){
-	  if(!error && response.statusCode == 200){
-	    console.log(body);
+	  if(error){
+	  	res.json(error);
 	  }
+	   res.json(body);
 	});
 });
 
 router.post('/add', function (req, res){
-
 	var amount = req.body.amount;
 	var purpose = req.body.purpose;
 	var payload = {
-  purpose: purpose,
-  amount: amount,
-  webhook: 'http://www.google.com/process/'
-};
+  	purpose: purpose,
+  	amount: amount,
+  	webhook: 'http://52.175.36.115:3000/redirect',
+  	redirect_url: 'http://52.175.36.115:3000/redirect'
+  	};
 
 console.log(payload);
 
